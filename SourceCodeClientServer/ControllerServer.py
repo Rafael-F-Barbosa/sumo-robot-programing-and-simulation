1bcec212420ba68711a92ba6a32574544f909766
Rafael Barbosa <rafaelfbarbosa7@gmail.com>
2021-10-14 23:16:04 -0300

    Started client-server idea

---
+++ SourceCodeClientServer/ControllerServer.py
@@ -0,0 +1,180 @@
+# Import modules 
+import sys  
+import time 
+import numpy as np
+import socket
+import threading
+ 
+
+# Adding directories for linux and mac
+if(sys.platform == "linux" or sys.platform == "linux2"):
+    sys.path.append('/home/rafael-barbosa/ptr_project/PyBinding')
+elif(sys.sys.platform == 'darwin'):
+    sys.path.append('/Users/admin/Documents/Mecatronica/8o semestre/PTR/Projeto 2/codigo')
+
+# Module to connect python to Coppelia
+import sim  
+
+
+# Create server connection and listen 
+HOST = 'localhost'
+PORT = 50011
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+try:
+    s.bind((HOST, PORT))
+except :
+    print("Failed to create server!")
+    sys.exit()
+s.listen()
+print("Controller waiting client...")
+# Connect to client
+conn, add = s.accept()
+print("Connected to", add)
+
+
+# Connect to CoppeliaSim
+# Just in case, close all opened connections
+sim.simxFinish(-1)
+
+# Connect to CoppeliaSim
+clientID = sim.simxStart('127.0.0.1', 19998, True, True, 5000, 5)
+
+# End program if connection failed
+if clientID != -1:
+    print('Connected to remote API server')
+else:
+    print("Failed to connect to remote API server")
+    print("Leaving program")
+    sys.exit()
+
+
+
+# Get motor handlers
+errorLeftMotor, leftMotor = sim.simxGetObjectHandle(
+    clientID, "Pioneer_p3dx_leftMotor", sim.simx_opmode_oneshot_wait)
+errorRightMotor, rightMotor = sim.simxGetObjectHandle(
+    clientID, "Pioneer_p3dx_rightMotor", sim.simx_opmode_oneshot_wait)
+
+# Print in handlers connections
+print("Handlers: (0 == alright)")
+print(errorLeftMotor, errorRightMotor)
+
+
+# Turn options right, left, 180 - Talvez não usaremos isso aqui
+def turn(direction, clientID, rightMotor, leftMotor):
+
+    time_multiplier = 5;
+    if(direction == 'left'):
+        setVelocity(-1*time_multiplier, time_multiplier, clientID, rightMotor, leftMotor)
+        time.sleep(1/time_multiplier)
+        setVelocity(0, 0, clientID, rightMotor, leftMotor)
+    elif(direction == 'right'):
+        setVelocity(time_multiplier, -1*time_multiplier, clientID, rightMotor, leftMotor)
+        time.sleep(1/time_multiplier)
+        setVelocity(0, 0, clientID, rightMotor, leftMotor)
+    elif(direction == '180'):
+        setVelocity(-1.15*time_multiplier, 1.15*time_multiplier, clientID, rightMotor, leftMotor)
+        time.sleep(1/time_multiplier)
+        setVelocity(0, 0, clientID, rightMotor, leftMotor)
+    elif(direction == 'back'):
+        setVelocity(1.15*time_multiplier, 1.15*time_multiplier, clientID, rightMotor, leftMotor)
+        time.sleep(1/time_multiplier)
+        setVelocity(0, 0, clientID, rightMotor, leftMotor)
+
+
+# Function to set a velocity
+def setVelocity(leftV, rightV, clientID, rightMotor, leftMotor):
+    sim.simxSetJointTargetVelocity(
+        clientID, leftMotor, leftV, sim.simx_opmode_oneshot_wait)
+    sim.simxSetJointTargetVelocity(
+        clientID, rightMotor, rightV, sim.simx_opmode_oneshot_wait)
+
+# Inicializei essa divisão aqui pra usar threads - mas n apliquei ainda
+def turn_right(direction, clientID, rightMotor, leftMotor):
+    time_multiplier = 5;
+    setVelocity(time_multiplier, -1*time_multiplier, clientID, rightMotor, leftMotor)
+    time.sleep(1/time_multiplier)
+    setVelocity(0, 0, clientID, rightMotor, leftMotor)
+
+def turn_left(direction, clientID, rightMotor, leftMotor):
+    time_multiplier = 5;
+    setVelocity(-1*time_multiplier, time_multiplier, clientID, rightMotor, leftMotor)
+    time.sleep(1/time_multiplier)
+    setVelocity(0, 0, clientID, rightMotor, leftMotor)
+
+def turn180(direction, clientID, rightMotor, leftMotor):
+    time_multiplier = 5;
+    setVelocity(-1.15*time_multiplier, 1.15*time_multiplier, clientID, rightMotor, leftMotor)
+    time.sleep(1/time_multiplier)
+    setVelocity(0, 0, clientID, rightMotor, leftMotor)
+
+def goBack(direction, clientID, rightMotor, leftMotor):
+    time_multiplier = 5;
+    setVelocity(1.15*time_multiplier, 1.15*time_multiplier, clientID, rightMotor, leftMotor)
+    time.sleep(1/time_multiplier)
+    setVelocity(0, 0, clientID, rightMotor, leftMotor)
+
+
+
+# Client requests
+clientRequests = ["Emergency.", "LineDetected.", "EnemyOnLeft.", "EnemyOnRight.", "EnemyOnFront.", "Break."]
+
+
+# Main ----------------------------------------------------------------------------------------------------
+sim.simxAddStatusbarMessage(clientID, "ControllerWaiting!", sim.simx_opmode_oneshot_wait)
+
+
+
+startTime = time.time()
+while(True):
+    # Go ahead
+    setVelocity(-4, -4, clientID, rightMotor, leftMotor)
+
+
+    # Receive sensor information
+    data = conn.recv(1024)
+
+    print(data)
+
+    # Processa data
+    if(data not in clientRequests):
+        tokens = data.decode().split(".")
+        correctData = tokens[-2] + "."
+    print("CorrectedData: ", correctData)
+
+    # Basic funcionality
+    if(correctData == "LineDetected."):
+        print("Detectou linha!!")
+        setVelocity(0, 0, clientID, rightMotor, leftMotor)
+        turn('back', clientID, leftMotor, rightMotor)
+        turn('180', clientID, leftMotor, rightMotor)
+        sim.simxAddStatusbarMessage(clientID, "Detectei linha!!!!", sim.simx_opmode_oneshot_wait)
+    elif(correctData == "Emergency."): # Emergência tá com prioridade menor que linha aqui, mas era só pra testar
+        sim.simxAddStatusbarMessage(clientID, "Emergência, irmão, para tudo!!!!", sim.simx_opmode_oneshot_wait)
+        print("Emergência - Finalizando programa")
+        break
+    elif(correctData == "EnemyOnLeft."):
+        turn('left', clientID, rightMotor,leftMotor)
+        print("Detectou esq!!")
+        sim.simxAddStatusbarMessage(clientID, "Adversário à esquerda!!!!", sim.simx_opmode_oneshot_wait)
+    elif(correctData == "EnemyOnRight."):
+        turn('right', clientID, rightMotor,leftMotor)
+        print("Detectou dire!!")
+        sim.simxAddStatusbarMessage(clientID, "Adversário à direita!!!!", sim.simx_opmode_oneshot_wait)
+    elif(correctData == "EnemyOnFront."):
+        setVelocity(-7, -7, clientID, rightMotor, leftMotor)
+        sim.simxAddStatusbarMessage(clientID, "Adversário à Frente!!!!", sim.simx_opmode_oneshot_wait)
+    elif(correctData == 'Break.' or not data):
+        setVelocity(0, 0, clientID, rightMotor, leftMotor) 
+        conn.close()
+        break
+
+# Statistics
+print("Program duration: ", time.time()-startTime)
+
+# Ends communication with server
+print("Server finished!")
+sim.simxAddStatusbarMessage(clientID, "ControllerFinished!", sim.simx_opmode_oneshot_wait)
+sim.simxFinish(clientID)
+
+
--- SourceCodeClientServer/InterruptionsHandlerClient.py
+++ SourceCodeClientServer/InterruptionsHandlerClient.py
@@ -2,17 +2,30 @@
 import sys  
 import time 
 import numpy as np
+import socket
  
-
 # Adding directories for linux and mac
 if(sys.platform == "linux" or sys.platform == "linux2"):
     sys.path.append('/home/rafael-barbosa/ptr_project/PyBinding')
 elif(sys.sys.platform == 'darwin'):
     sys.path.append('/Users/admin/Documents/Mecatronica/8o semestre/PTR/Projeto 2/codigo')
 
-# Interface between python and Coppelia
+# Connects python to Coppelia
 import sim  
 
+
+# Connect to server controler
+HOST = 'localhost'
+PORT = 50011
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+try:
+    s.connect((HOST, PORT))
+except:
+    print("Failed to connect to server controller")
+    sys.exit()
+
+
+# Connect to CoppeliaSim
 # Just in case, close all opened connections
 sim.simxFinish(-1)
 
@@ -27,15 +40,6 @@ else:
     print("Leaving program")
     sys.exit()
 
-# Show message in coppelia
-sim.simxAddStatusbarMessage(
-    clientID, "Python running!", sim.simx_opmode_oneshot)
-
-# Get motor handlers
-errorLeftMotor, leftMotor = sim.simxGetObjectHandle(
-    clientID, "Pioneer_p3dx_leftMotor", sim.simx_opmode_oneshot_wait)
-errorRightMotor, rightMotor = sim.simxGetObjectHandle(
-    clientID, "Pioneer_p3dx_rightMotor", sim.simx_opmode_oneshot_wait)
 
 # Get sensor handlers
 errorLeftSensor, leftSensor = sim.simxGetObjectHandle(
@@ -53,44 +57,14 @@ errorInfraredSensor, infraredSensor = sim.simxGetObjectHandle(
 errorEmergencySensor, emergencySensor = sim.simxGetObjectHandle(
     clientID, "EmergencySensor", sim.simx_opmode_oneshot_wait)
 
-# Temperature sensor
+# Temperature sensor - Esse aqui acho que vai ficar no controller mesmo
 errorTemperatureSensor, temperatureSensor = sim.simxGetObjectHandle(
     clientID, "TemperatureSensor", sim.simx_opmode_oneshot_wait)
 
-
 # Print in handlers connections
 print("Handlers: (0 == alright)")
-print(errorLeftMotor, errorRightMotor, errorLeftSensor, errorRightSensor, errorFrontSensor, errorInfraredSensor, errorEmergencySensor, errorTemperatureSensor)
+print(errorLeftSensor, errorRightSensor, errorFrontSensor, errorInfraredSensor, errorEmergencySensor, errorTemperatureSensor)
 
-
-# Function to set a velocity
-def setVelocity(leftV, rightV, clientID, rightMotor, leftMotor):
-    sim.simxSetJointTargetVelocity(
-        clientID, leftMotor, leftV, sim.simx_opmode_oneshot_wait)
-    sim.simxSetJointTargetVelocity(
-        clientID, rightMotor, rightV, sim.simx_opmode_oneshot_wait)
-
-
-# Turn options right, left, 180
-def turn(direction, clientID, rightMotor, leftMotor):
-
-    time_multiplier = 5;
-    if(direction == 'left'):
-        setVelocity(-1*time_multiplier, time_multiplier, clientID, rightMotor, leftMotor)
-        time.sleep(1/time_multiplier)
-        setVelocity(0, 0, clientID, rightMotor, leftMotor)
-    elif(direction == 'right'):
-        setVelocity(time_multiplier, -1*time_multiplier, clientID, rightMotor, leftMotor)
-        time.sleep(1/time_multiplier)
-        setVelocity(0, 0, clientID, rightMotor, leftMotor)
-    elif(direction == '180'):
-        setVelocity(-1.15*time_multiplier, 1.15*time_multiplier, clientID, rightMotor, leftMotor)
-        time.sleep(1/time_multiplier)
-        setVelocity(0, 0, clientID, rightMotor, leftMotor)
-    elif(direction == 'back'):
-        setVelocity(1.15*time_multiplier, 1.15*time_multiplier, clientID, rightMotor, leftMotor)
-        time.sleep(1/time_multiplier)
-        setVelocity(0, 0, clientID, rightMotor, leftMotor)
 
 # Read sensors first time
 def initializeSensors(clientID, leftSensor, rightSensor, frontSensor, infraredSensor, emergencySensor, temperatureSensor):
@@ -112,47 +86,15 @@ def initializeSensors(clientID, leftSensor, rightSensor, frontSensor, infraredSe
         clientID, temperatureSensor, sim.simx_opmode_streaming)
 
 
-# Function to read temperature
-def readTemperature(clientID, temperatureSensor):
-    returnCodeTemp, detectionStateTemp, detectedPointTemp, detectedObjectHandTemp, detectedSurfaceNormalVectorTemp = sim.simxReadProximitySensor(
-        clientID, temperatureSensor, sim.simx_opmode_buffer)
-    if(detectionStateTemp): 
-        sim.simxAddStatusbarMessage(clientID, "TEMPERATURA ALTA, irmão, para tudo!!!!", sim.simx_opmode_oneshot_wait)
-        print("Temperatura alta - Finalizando programa")
-        return 1
-    print('LI TEMPERATURA')
-    sim.simxAddStatusbarMessage(clientID, "LI TEMPERATURA", sim.simx_opmode_oneshot_wait)
-
-    return 0
-
-
 # Main ----------------------------------------------------------------------------------------------------
-sim.simxAddStatusbarMessage(clientID, "Main program started!", sim.simx_opmode_oneshot_wait)
-
 initializeSensors(clientID, leftSensor, rightSensor, frontSensor, infraredSensor, emergencySensor, temperatureSensor)
+sim.simxAddStatusbarMessage(clientID, "SensorsInitialized!", sim.simx_opmode_oneshot_wait)
 
-# Period - Read Temperature
-pReadTemperature = 3
-t1 = 0
-t2 = 0
 
-# Simulate for 60 seconds
+
+# Simulate for some seconds
 finalTime = time.time() + 30
 while(time.time() < finalTime):
-    
-    # Go ahead
-    setVelocity(-4, -4, clientID, rightMotor, leftMotor)
-
-    # Read Temperature  - IMPOSSIVEL FAZER POR THREAD
-    t1 = time.time()
-    if (t1 - t2) > 3:
-        high = readTemperature(clientID, temperatureSensor)
-        t2 = time.time()
-        if high:
-            break
-
-
-
     # Read sensors
     returnCode, resolution, image = sim.simxGetVisionSensorImage(clientID,infraredSensor, 1, sim.simx_opmode_buffer)
     returnCodeLeft, detectionStateLeft, detectedPointLeft, detectedObjectHandLeft, detectedSurfaceNormalVectorLeft = sim.simxReadProximitySensor(
@@ -161,36 +103,29 @@ while(time.time() < finalTime):
         clientID, rightSensor, sim.simx_opmode_buffer)
     returnCodeFront, detectionStateFront, detectedPointFront, detectedObjectHandFront, detectedSurfaceNormalVectorFront = sim.simxReadProximitySensor(
         clientID, frontSensor, sim.simx_opmode_buffer)
-    
     # Emergency Sensor 
     returnCodeEmergency, detectionStateEmergency, detectedPointEmergency, detectedObjectHandEmergency, detectedSurfaceNormalVectorEmergency = sim.simxReadProximitySensor(
         clientID, emergencySensor, sim.simx_opmode_buffer)
 
 
 
-    # Basic funcionality
-    if(abs(np.mean(image)) < 30):
-        setVelocity(0, 0, clientID, rightMotor, leftMotor)
-        turn('back', clientID, leftMotor, rightMotor)
-        turn('180', clientID, leftMotor, rightMotor)
-        sim.simxAddStatusbarMessage(clientID, "Detectei linha!!!!"+"-"+str(np.mean(image)), sim.simx_opmode_oneshot_wait)
-    elif(detectionStateEmergency): # Emergência tá com prioridade menor que linha aqui, mas era só pra testar
-        sim.simxAddStatusbarMessage(clientID, "Emergência, irmão, para tudo!!!!", sim.simx_opmode_oneshot_wait)
-        print("Emergência - Finalizando programa")
+    # Sending information
+    if(detectionStateEmergency):
+        s.send(str.encode("Emergency."))
         break
+    elif(abs(np.mean(image)) < 30):
+        s.send(str.encode("LineDetected."))
     elif(detectionStateLeft):
-        turn('left', clientID, rightMotor,leftMotor)
-        sim.simxAddStatusbarMessage(clientID, "Adversário à esquerda!!!!"+"-"+str(np.mean(image)), sim.simx_opmode_oneshot_wait)
+        s.send(str.encode("EnemyOnLeft."))
     elif(detectionStateRight):
-        turn('right', clientID, rightMotor,leftMotor)
-        sim.simxAddStatusbarMessage(clientID, "Adversário à direita!!!!"+"-"+str(np.mean(image)), sim.simx_opmode_oneshot_wait)
+        s.send(str.encode("EnemyOnRight."))
     elif(detectionStateFront):
-        setVelocity(-7, -7, clientID, rightMotor, leftMotor)
-        sim.simxAddStatusbarMessage(clientID, "Adversário à Frente!!!!"+"-"+str(np.mean(image)), sim.simx_opmode_oneshot_wait)
-    
+        s.send(str.encode("EnemyOnFront."))
 
-# Stop robot
-setVelocity(0, 0, clientID, rightMotor, leftMotor) 
+    time.sleep(0.01)
 
-# Ends communication with server
-sim.simxFinish(clientID)
+
+# Ending program client and server
+print("End program")
+sim.simxAddStatusbarMessage(clientID, "SensorsFinished!", sim.simx_opmode_oneshot_wait)
+s.send(str.encode("Break."))
